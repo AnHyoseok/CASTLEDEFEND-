08fbc8b399f55074537fd58d59c9ea13bef55d52
rmsepskek02 <rmsepskek02@gmail.com>
2024-11-22 18:26:06 +0900

    Tower Scripts 정리
    1) 각종 타워 스크립트 추가
    2) Layer에 Boss 추가
     => Constants.cs에 Boss 추가
    3) TowerInfo 추가
     => Tower의 정보를 담고 있는 스크립트
    4) 각 스크립트 namespace 추가
    5) Scripts 폴더링

--- Assets/Scripts/Constants.cs
+++
@@ -1,9 +0,0 @@
-/// <summary>
-/// 게임 내 고정된 상수를 정의 한 클래스
-/// </summary>
-public static class Constants 
-{
-    #region Layer 
-    public static string enemyLayer = "Enemy";                      //Layer - Enemy 
-    #endregion
-}
