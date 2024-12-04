739ef917b787c002bb51d26f71041aee17cc18da
dksgytjr10 <dksgytjr10@gmail.com>
2024-12-04 17:22:56 +0900

    1.아이템드랍구현

--- Assets/Prefabs/HyoSeok/Scripts/Interactive/DropItem.cs
+++ Assets/Prefabs/HyoSeok/Scripts/Interactive/DropItem.cs
@@ -10,13 +10,16 @@ namespace Defend.item
     {
         #region Variables
         private PlayerState playerState;
+        private GameObject targetObject;
         public string resourceName; // 자원이름
         public float amount; // 자원 양
 
         //아이템 흭득 사운드
         public AudioClip GetItemSound;
         private AudioSource audioSource;
-       
+
+        //자원 중복흭득방지
+        private bool isCollected = false;
         #endregion
 
         private void Awake()
@@ -24,7 +27,7 @@ namespace Defend.item
             Collider collider = GetComponent<Collider>();
             playerState = Object.FindAnyObjectByType<PlayerState>(); // PlayerState를 찾기
             audioSource = gameObject.AddComponent<AudioSource>();
-          
+            targetObject = GameObject.Find("PlayerBody");
 
         }
        
@@ -40,11 +43,20 @@ namespace Defend.item
         {
             if (playerState != null)
             {
-                float distanceToPlayer = Vector3.Distance(transform.position, playerState.transform.position);
+                float distanceToPlayer = Vector3.Distance(transform.position, targetObject.transform.position);
                 if (distanceToPlayer < ResourceManager.distance)
                 {
-                    Vector3 direction = (playerState.transform.position - transform.position).normalized;
+                    Vector3 direction = (targetObject.transform.position - transform.position).normalized;
+            
                     transform.position += direction * ResourceManager.speed * Time.deltaTime;
+
+                    if (distanceToPlayer < 0.5f && !isCollected) 
+                    {
+                        GetResource();
+                        audioSource.clip = GetItemSound;
+                        audioSource.Play();
+                        Destroy(gameObject, 0.1f);
+                    }
                 }
             }
         }
@@ -53,14 +65,15 @@ namespace Defend.item
         {
             // 플레이어에게 전달
             ResourceManager.Instance.AddResources(amount, resourceName);
+            isCollected = true;
         }
 
         //닿으면 제거
         private void OnTriggerEnter(Collider other)
         {
-            
+            //Debug.Log(other.gameObject);
 
-            if (other.CompareTag("Player"))
+            if (other.CompareTag("Player") && !isCollected)
             {
                 GetResource();
                 audioSource.clip = GetItemSound;
