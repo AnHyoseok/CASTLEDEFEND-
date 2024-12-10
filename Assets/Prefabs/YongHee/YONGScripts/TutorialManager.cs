0813d282f8df708aa9bc8679f546ec1a92bf47ce
rmsepskek02 <rmsepskek02@gmail.com>
2024-12-10 17:44:54 +0900

    Tutorial 작업중
    Tutorial 작업중

---
+++ Assets/Prefabs/YongHee/YONGScripts/TutorialManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+/// <summary>
+/// Tutorial Scene을 관리하는 Manager
+/// </summary>
+namespace Defend.Tutorial
+{
+    public class TutorialManager : MonoBehaviour
+    {
+        // 이동하기
+        // 장비바꾸기
+        // 자원캐기
+
+        // 각종 왼손 UI 기능 TEST
+        // 무슨 기능이 있는지 ?
+
+        // 타워 짓기 
+        
+        // 라운드 시작은 어떻게 ?
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
