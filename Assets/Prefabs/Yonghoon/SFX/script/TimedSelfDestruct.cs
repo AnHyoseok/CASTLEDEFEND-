c1f215cf1de54f8c2af58650a1319f59c9634db3
toneed <dydgns0729@gmail.com>
2024-12-13 16:44:54 +0900

    1. AudioUtility, AuidoManager 구현 (전체적인 사운드 관리 및 효과음 발생 로직 구현) 2. EnemyMoveSound(Warrior,Wizard) Test

--- Assets/Prefabs/Yonghoon/SFX/script/TimedSelfDestruct.cs
+++ Assets/Prefabs/Yonghoon/SFX/script/TimedSelfDestruct.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 namespace Defend.Utillity
 {
+    /// <summary>
+    /// 오디오(SFX[Effect]) 효과 재생시 AudioUtility.cs -> AudioCreateSFX()를 통해
+    /// 빈 오브젝트를 만들어 AudioSource(Audio Clip)를 붙여주고 사운드를 재생함
+    /// 사운드의 길이만큼 재생을 하고 만들어졌던 빈 오브젝트는 Destroy해주는 역할
+    /// LifeTime -> AudioClip.length 를 통해 변수를 초기화함
+    /// </summary>
     public class TimedSelfDestruct : MonoBehaviour
     {
         public float LifeTime = 1f;
