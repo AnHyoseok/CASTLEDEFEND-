95edc3fce5c9602b6c77d3aaf5e45c3f3daf3e0e
toneed <dydgns0729@gmail.com>
2024-12-23 15:05:40 +0900

    1. Enemy, Boss Physics Settings 전부 비활성화체크 (충돌무시)

--- Assets/Prefabs/Yonghoon/Scripts/Enemy/EnemyMoveController.cs
+++ Assets/Prefabs/Yonghoon/Scripts/Enemy/EnemyMoveController.cs
@@ -136,7 +136,7 @@ namespace Defend.Enemy
 
         private void UpdateCurrentSpeed()
         {
-            Debug.Log("속도변경 호출!");
+            //Debug.Log("속도변경 호출!");
             float totalRate = 0;
 
             // 모든 슬로우 비율을 합산
